c008785b8ce463de1e836b06f2ec0159eb288b83
Shaurya Sethi <shauryaswapansethi10@gmail.com>
2025-08-30 22:00:55 +0530

    feat: Implement file reading from command-line argument
    Replace the "Hello, world!" placeholder with the core functionality of the application.
    
    The program now accepts a single command-line argument specifying a file path. It reads the given file line by line and prints its contents to standard output.
    
    This change includes robust error handling for:
    - Incorrect number of arguments
    - File not found errors
    - Other I/O errors during file reading
    
    The program now uses appropriate process exit codes to signal success or failure. Additionally, `TODOS.md` is added to `.gitignore`.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+use std::fs::File;
+use std::process;
+use std::io::{BufReader, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+   let args: Vec<String> = env::args().collect(); // Collect file path
+
+   if args.len() != 2 {  // 
+    eprintln!("Usage: read-rs <FILE_PATH>");
+    process::exit(1)
+   }
+
+   let path = &args[1]; 
+
+   let file_result = File::open(path);
+   let file = match file_result {
+    Ok(file) => file,
+    Err(error) => {
+        match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                eprintln!("File not found: {}", error);
+                process::exit(1)
+            }
+            _ => {
+                eprintln!("Error opening file: {}", error);
+                process::exit(2)
+            }
+        }
+    }
+   };
+
+   let reader = BufReader::new(file);
+   for line in reader.lines() {
+    match line {
+        Ok(line) => println!("{}", line),
+        Err(error) => {
+            eprintln!("Error reading line: {}", error);
+            process::exit(2)
+        }
+    }
+   }
+   process::exit(0)
 }
